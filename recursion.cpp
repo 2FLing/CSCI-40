00507caacbb04ae34effbb14ae92bde762402f6e
Mingkuan Pang <39754206+2FLing@users.noreply.github.com>
2019-05-16 19:37:42 -0700

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -8,7 +8,59 @@ using namespace std;
 int factorial(int);
 int gcf(int, int);
 int main()
+{//MingkuanPang
+//This program can caculate factorials
+// n! = (n-1)! x n and the greatest common factor
+//of two numbers.
+#include<iostream>
+#include<limits.h>
+using namespace std;
+int factorial(int);
+int gcf(int, int);
+int main()
 {
+	int times, number1, number2, result1, result2;
+	cout << "How many times of factorial do you want to see?" << endl;
+	cin >> times;
+	cout << "What numbers greatest common factor do you want to see?" << endl;
+	cin >> number1 >> number2;
+	result1 = factorial(times);
+	result2 = gcf(number1, number2);
+	cout << "The result of " << times << " times of factorial is: " << result1 << endl;
+	cout << "The greatest common factor of " << number1 << " and " << number2 << " is: " << result2 << endl;
+	return 0;
+}
+//This function can caculate the factorial base on the times
+//taht the user input.
+int factorial(int times)
+{
+	if (times == 0)
+		times = 1;
+	else
+	{
+		if (factorial(times - 1) > (INT_MAX / times))
+		{
+			times = 0;
+		}
+		else
+			times = factorial(times - 1) * times;
+	}
+	return times;
+}
+//This function can caculate the greatest common factor of two numbers.
+int gcf(int number1, int number2)
+{
+	int temp;
+	if (number2 != 0)
+	{
+		temp = number2;
+		number2 = number1 % number2;
+		number1 = temp;
+		number1=gcf(number1, number2);
+	}
+	return number1;
+}
+
 	int times, number1, number2, result1, result2;
 	cout << "How many times of factorial do you want to see?" << endl;
 	cin >> times;
